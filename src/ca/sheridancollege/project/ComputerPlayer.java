8931aa8766bd8845a00637b3e15401cfe2128fd7
ammar <ammar@DESKTOP-DC3QBUG.mshome.net>
2021-04-11 17:17:40 -0400

    BIG i think Final Changes

--- src/ca/sheridancollege/project/ComputerPlayer.java
+++ src/ca/sheridancollege/project/ComputerPlayer.java
@@ -14,22 +14,22 @@ public class ComputerPlayer extends Player{
         System.out.println("Computer is picking...");
         try {
             Thread.sleep(2000);
-        }catch(InterruptedException ignored){
+        } catch (InterruptedException ignore) {
         }
+
         int numChosen = (int) Math.floor(Math.random() * (playerHand.getHandSize()-1));
 
         while(game.requestCard(playerHand.getCard(numChosen), this)) {
             numChosen = (int) Math.floor(Math.random() * (playerHand.getHandSize()-1));
             System.out.println("Request Completed Computer has taken a Card From you and will request again: "
-                    + playerHand.getCard(playerHand.getHandSize() - 1)+ "\n");
+                    + playerHand.getCard(playerHand.getHandSize() - 1));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ignore) {
             }
-
-            System.out.println("Computer's Request Failed now will draw a card \n");
-            playerHand.drawCard();
         }
+        System.out.println("Computer's Request Failed now will draw a card \n");
+        playerHand.drawCard();
 
         Card temp = playerHand.getCard(playerHand.getHandSize() - 1);
 
@@ -37,7 +37,5 @@ public class ComputerPlayer extends Player{
             addPoint();
             playerHand.extractCard(temp.rank);
         }
-
-
     }
 }
